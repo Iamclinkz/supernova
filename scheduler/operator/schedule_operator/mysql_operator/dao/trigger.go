2938a032c877f6e43f1dda52f9d921530214224e
exiasun <exiasun@tencent.com>
2023-07-30 22:23:52 +0800

    fix dao

--- scheduler/operator/schedule_operator/mysql_operator/dao/trigger.go
+++ scheduler/operator/schedule_operator/mysql_operator/dao/trigger.go
@@ -5,25 +5,24 @@ import (
 	"supernova/scheduler/constance"
 	"supernova/scheduler/model"
 	"time"
-
-	"gorm.io/gorm"
 )
 
 type Trigger struct {
-	gorm.Model
-	Name              string
-	JobID             uint
-	ScheduleType      constance.ScheduleType
-	ScheduleConf      string
-	MisfireStrategy   constance.MisfireStrategyType
-	FailRetryCount    int
-	ExecuteTimeout    time.Duration
-	FailRetryInterval time.Duration
-	TriggerLastTime   time.Time
-	TriggerNextTime   time.Time
-	Status            constance.TriggerStatus
-	Param             string
-	AtLeastOnce       bool
+	ID                uint                          `gorm:"column:id;primarykey"`
+	UpdatedAt         time.Time                     `gorm:"column:updated_at"`
+	Name              string                        `gorm:"column:name;type:varchar(64)"`
+	JobID             uint                          `gorm:"not null;index"`
+	ScheduleType      constance.ScheduleType        `gorm:"column:schedule_type;type:tinyint(4);not null"`
+	ScheduleConf      string                        `gorm:"column:schedule_conf;type:varchar(128)"`
+	MisfireStrategy   constance.MisfireStrategyType `gorm:"column:misfire_strategy;type:tinyint(4);not null"`
+	FailRetryCount    int                           `gorm:"column:executor_fail_retry_count;not null"`
+	ExecuteTimeout    time.Duration                 `gorm:"column:execute_timeout;type:bigint;not null"`
+	FailRetryInterval time.Duration                 `gorm:"column:fail_retry_interval"`
+	TriggerLastTime   time.Time                     `gorm:"column:trigger_last_time;type:timestamp;not null"`
+	TriggerNextTime   time.Time                     `gorm:"column:trigger_next_time;type:timestamp;not null;index"`
+	Status            constance.TriggerStatus       `gorm:"column:status;type:tinyint(4);not null"`
+	Param             string                        `gorm:"column:param"`
+	AtLeastOnce       bool                          `gorm:"column:at_least_once"`
 }
 
 func (t *Trigger) TableName() string {
@@ -37,10 +36,8 @@ func FromModelTrigger(mTrigger *model.Trigger) (*Trigger, error) {
 	}
 
 	return &Trigger{
-		Model: gorm.Model{
-			ID:        mTrigger.ID,
-			UpdatedAt: mTrigger.UpdatedAt,
-		},
+		ID:                mTrigger.ID,
+		UpdatedAt:         mTrigger.UpdatedAt,
 		Name:              mTrigger.Name,
 		JobID:             mTrigger.JobID,
 		ScheduleType:      mTrigger.ScheduleType,
